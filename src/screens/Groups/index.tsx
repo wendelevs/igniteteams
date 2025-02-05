4d8b69c0f86dcc4c3e395ed401b7cd3f247b4fff
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-05 18:33:13 -0300

    CSS-in-JS - Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
 import { Container } from "./styles";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
-      <Highlight 
-        title="Turmas" 
-        subtitle="Jogue com a sua turma"/>
-      <GroupCard 
-        title="Galera do Ignite"
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} />
+        )}
       />
     </Container>
   );
