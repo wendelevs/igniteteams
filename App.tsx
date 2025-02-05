f3249beaad6aa960c5daaf6c7a494dab256814ff
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-05 17:10:11 -0300

    CSS-in-JS - Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 import { Groups } from "./src/screens/Groups";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
