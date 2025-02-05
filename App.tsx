1710ca33075ac32232296f132ca2e9fe8a07f00d
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-05 16:51:24 -0300

    CSS-in-JS - Utilizando o tema

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
-import { ThemeProvider } from "styled-components";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
-import { Groups } from "@screens/Groups";
+import { Groups } from "./src/screens/Groups";
 
 export default function App() {
   return (
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,13 +1,9 @@
 import styled from "styled-components/native";
+import { DefaultTheme } from "styled-components";
+import "styled-components";
 
 export const Container = styled.View`
     flex: 1;
-    background-color: #000;
-    align-items: center;
-    justify-content: center;
-`;
-
-export const Title = styled.Text`
-    color: #FFF;
-    font-size: 32px;
+    background-color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_600};
+    padding: 24px;
 `;
