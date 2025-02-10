ec23b6a1e31f0052b8033f7638c518ad3efb488a
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-10 17:16:51 -0300

    Navegação - Utilizando a SafeAreaView

--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,7 +1,8 @@
 import { DefaultTheme } from "styled-components";
 import styled, { css } from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_600};
     padding: 24px;
