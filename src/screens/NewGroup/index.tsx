69a9902bbeb058b87fae5ce11d54af1ad0cc5c14
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-10 17:07:08 -0300

    Navegação - Implementando o voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,15 @@ import { Container, Content, Icon } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group: "Rocket"});
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -20,6 +27,7 @@ export function NewGroup() {
         <Button 
             title="Criar"
             style={{ marginTop: 20 }}
+            onPress={handleNew}
         />
       </Content>
     </Container>
