54a0eea9e6cf5865dda9fd00ff26472119068ab2
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-10 11:22:40 -0300

    CSS-in-JS - Criando interface NewGroup

---
+++ src/screens/NewGroup/styles.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from "styled-components";
+import styled from "styled-components/native";
+import { UsersThree } from "phosphor-react-native";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: ${({ theme }: { theme: DefaultTheme}) => theme.COLORS.GRAY_600};
+    padding: 24px;
+`;
+
+export const Content = styled.View`
+    flex: 1;
+    justify-content: center;
+`;
+
+export const Icon = styled(UsersThree).attrs(({ theme }: { theme: DefaultTheme }) => ({
+    size: 56,
+    color: theme.COLORS.GREEN_700
+}))`
+    align-self: center;
+`;
