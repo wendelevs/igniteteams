5e8982eb8c0e75d64a7fba841bdaac898a0f5900
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-10 14:53:32 -0300

    CSS-in-JS - Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
 import { Header } from "@components/Header";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Highlight } from "@components/Highlight";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+  const [team, setTeam] = useState("Time A");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,7 +22,23 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
-      <Filter title="Time A"/>
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -14,3 +14,18 @@ export const Form = styled.View`
     justify-content: center;
     border-radius: 6px;
 `;
+
+export const HeaderList = styled.View`
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    margin: 32px 0 12px;
+`;
+
+export const NumbersOfPlayers = styled.Text`
+    ${({ theme }: { theme: DefaultTheme }) => css`
+        color: ${theme.COLORS.GRAY_200};
+        font-family: ${theme.FONT_FAMILY.BOLD};
+        font-size: ${theme.FONT_SIZE.SM}px;
+    `};
+`;
